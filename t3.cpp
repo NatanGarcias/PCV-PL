54b05ccc876c8a26dc0f6bb014c5c07f2c5cf352
NatanGarcias <natans.garcias@gmail.com>
2019-12-10 15:43:36 -0300

    a

---
+++ t3.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int nV;
+int nP;
+int C;
+int Q;
+
+struct Pedagio{
+    long double desconto;
+    int saida;
+    int chegada;
+};
+
+int main(){
+    
+    leitura();
+}
